98875a5dba9e9697a8333b842cfaeff27ae5b612
Douglas Medina <124748350+douglas-medina@users.noreply.github.com>
2023-05-11 12:46:20 -0300

    commit inicial

---
+++ gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        less: {
+            development: { //para ambiente de desenvolvimento
+                files: {
+                    'dev/styles/main.css': 'src/styles/main.less' //arquivo final : arquivo de origem
+                }
+            },
+            production: { //para ambiente de produção comprimido (site em si)
+                options: {
+                    compress: true,
+                },
+                files: {
+                    'dist/styles/main.min.css': 'src/styles/main.less'
+                }
+            }
+        },
+
+        uglify: {
+            target: {
+                files: {
+                    'dist/scripts/main.min.js': 'src/scripts/main.js'
+                }
+            }
+        }
+    })
+
+    grunt.loadNpmTasks('grunt-contrib-less'); //compacta o less
+    grunt.loadNpmTasks('grunt-contrib-uglify');  //comprime js
+    
+}
